a9682f38d8032add72f26e694c63a74a20a74f52
Rozie Roz <rosewabere4@gmail.com>
2025-03-24 11:33:04 +0300

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,14 @@ export default defineConfig({
     commonjsOptions: {
       include: [/node_modules/],
       strictRequires: true
+    },
+    assetsInlineLimit: 4096, // 4kb - files smaller than this will be inlined as base64
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
     }
-  }
+  },
+  base: '/' // Ensures assets are referenced from the root path
 })
